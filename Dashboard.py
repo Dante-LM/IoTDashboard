c0624d6756a5284d0431773343e0ecd8409427cb
Christian <christianpauld002@gmail.com>
2021-11-11 07:10:16 -1000

    added all the stuff for email and motor but not integrate it

--- Dashboard.py
+++ Dashboard.py
@@ -4,25 +4,33 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import RPi.GPIO as GPIO
-import board
-import adafruit_dht
+import board, adafruit_dht
+import smtplib, ssl, email, imaplib
+from time import sleep
+import time
 
 #external_stylesheets = ['']
 
+global currentTemp
+global currentHumidity
 global led
-global tempGauge
-global humGauge
+global tempThresh
+global lightThresh
 
-dhtDevice = adafruit_dht.DHT11(board.D24)
+dhtDevice = adafruit_dht.DHT11(board.D26)
 
 app = dash.Dash(__name__)
 
-led = 13
-
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(led, GPIO.OUT)
 
+Motor1A = 17
+Motor1B = 27
+Motor1E = 22
+
+led = 13
+ 
+GPIO.setup(led, GPIO.OUT)
 
 def ledToggle(n_clicks):
     
@@ -42,6 +50,55 @@ def getDHTinfo():
     currentHumidity = dhtDevice.humidity
     info = [currentTemp, currentHumidity]
     return info
+
+def sendEmail(text):
+    sender ="dantelomonaco.iot@gmail.com"
+    password = "vanieriotemail321"
+    receiver = "dantelomonaco.iot2@gmail.com"
+    port = 465
+    subject = "From Dante's RPI"
+    message = 'Subject: {}\n\n{}'.format(subject, text)
+    context = ssl.create_default_context()
+    print("sending")
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender, password)
+        server.sendmail(sender, receiver, message)
+        print("sent email!")
+
+def receiveEmailChecker() :
+    while True:
+        mail = imaplib.IMAP4_SSL('imap.gmail.com')
+        mail.login('dantelomonaco.iot@gmail.com', 'vanieriotemail321')
+        mail.list()
+        mail.select("inbox") # connect to inbox.
+        result, data = mail.search(None, "(UNSEEN)")
+
+        ids = data[0]
+        id_list = ids.split() 
+        if(len(id_list)) > 0:
+            latest_email_id = id_list[-1] 
+        else:
+            return "Nothing"
+        result, data = mail.fetch(latest_email_id, "(RFC822)")
+        raw_email = data[0][1] 
+        decoded_data = email.message_from_string(raw_email.decode("utf-8"))
+        if type(decoded_data.get_payload()[0]) is str:
+            break
+        else:
+            text = decoded_data.get_payload()[0].get_payload();
+            answer1 = text[0:3].lower()
+            answer2 = text[0:2].lower()
+            if(answer1 == "yes"):
+                GPIO.setup(Motor1A,GPIO.OUT)
+                GPIO.setup(Motor1B,GPIO.OUT)
+                GPIO.setup(Motor1E,GPIO.OUT)
+                GPIO.output(Motor1A, GPIO.HIGH)
+                return "yes"
+            elif(answer1 == "no"):
+                return "no"
+            else:
+                return "Invalid"
 
     
 info = getDHTinfo()
@@ -85,17 +142,21 @@ app.layout = html.Div(children=[
     html.Button('Toggle Light', id='led_button', value='toggle_light', n_clicks=0),
     html.Div(id='led_output'),
     html.Br(),
+    
     html.H2(children='Fan Control'),
-    dcc.Input(type='number'),
-    html.Button('Confirm Threshold', id='fan_threshold', value='fan_control', n_clicks=0),
+    dcc.Input(id='fan_input', type='number'),
+    html.Button('Confirm Threshold', id='fan_button', value='fan_control', n_clicks=0),
     html.Br(),
+    
     html.H2(children='Automatic Light Control'),
     dcc.Input(type='number'),
     html.Button('Confirm Threshold', id='light_threshold', value='light_control', n_clicks=0),
     html.Br(),
+    
     dcc.Graph(id='tempGauge', figure=tempGauge),
     dcc.Graph(id='humGauge', figure=humGauge),
-    dcc.Interval(id = 'intervalComponent', interval = 1 * 3000, n_intervals = 0)
+    dcc.Interval(id = 'intervalComponent', interval = 1 * 3000, n_intervals = 0),
+    html.Div(id='hidden-div', style={'display':'none'})
 ])
 
 
@@ -105,6 +166,15 @@ app.layout = html.Div(children=[
 )
 def update_led_output_div(n):
     return '{}'.format(str(ledToggle(n)))
+
+# @app.callback(
+#     Output('hidden-div', 'children'),
+#     [Input('fan_button', 'n_clicks')],
+#     [State('fan_input','value')],
+#     )
+# def update_threshhold_output_div(clicks, input_value):
+#     if clicks is not None:
+#         print(clicks, input_value)
 
 
 @app.callback([
